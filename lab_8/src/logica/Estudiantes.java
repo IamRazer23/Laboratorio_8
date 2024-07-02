0d7cf853e8d7f8348dfd1a2b2efec561a0904f60
IamRazer23 <botaciojuan3@gmail.com>
2024-07-01 21:07:30 -0500

    First Commit

---
+++ lab_8/src/logica/Estudiantes.java
@@ -0,0 +1,35 @@
+package logica;
+
+
+
+public class Estudiantes {
+	private String nombre;
+    private String cedula;
+    private String carrera;
+    private double indiceAcademico;
+    private String sexo;
+
+    public Estudiantes(String nombre, String cedula, String carrera, double indiceAcademico, String sexo) {
+        if (indiceAcademico < 0.0 || indiceAcademico > 3.0) {
+            throw new IllegalArgumentException("El índice académico debe estar entre 0.0 y 3.0");
+        }
+        
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.carrera = carrera;
+        this.indiceAcademico = indiceAcademico;
+        this.sexo = sexo;
+    }
+
+    public double getIndiceAcademico() {
+        return indiceAcademico;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public String getSexo() {
+        return sexo;
+    }
+}
